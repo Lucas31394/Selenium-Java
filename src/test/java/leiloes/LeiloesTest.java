47b34fecd8926a686c3777ccd77302805e841618
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-09-16 10:37:09 -0300

    Carregando o formulário de cadastro de leilão

---
+++ src/test/java/leiloes/LeiloesTest.java
@@ -0,0 +1,25 @@
+package leiloes;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.leilao.login.LoginPage;
+
+public class LeiloesTest {
+	
+	private LeiloesPage paginaDeLeiloes;
+	
+	@AfterEach
+	public void afterEach() {
+		this.paginaDeLeiloes.fechar();
+	}
+	
+	@Test
+	public void deveriaCadastrarLeilao() {
+		LoginPage paginaDeLogin = new LoginPage();
+		paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
+		this.paginaDeLeiloes = paginaDeLogin.submeteFormulario();
+		CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+	}
+
+}
